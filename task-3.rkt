6b26ec4bbbd4961a22093e06c60d2a6126780d6f
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-01 18:45:43 -0400

    reduce macros to min, use h-o functions instead; make executable || work around for gregor bug

--- task-3.rkt
+++ task-3.rkt
@@ -1,3 +1,4 @@
+#! /usr/bin/env gracket
 #lang racket/gui
 
 ;; a flight booker that allows a choice between one-way and return bookings
@@ -6,8 +7,18 @@
 ;; ---------------------------------------------------------------------------------------------------
 (require gregor)
 
-;; this should be fixed in gregor 
+;; this should be fixed in gregor
+(define (to-date d)
+  (define matched (regexp-match #px"(\\d\\d|\\d).(\\d\\d|\\d).(\\d\\d\\d\\d)" d))
+  (and matched
+       (let-values ([(dd mm yy) (apply values (map string->number (rest matched)))])
+         (with-handlers ([exn? (λ (_) #f)]) (date yy mm dd)))))
+
+#;
 (define (to-date d) (with-handlers ([exn? (λ (_) #f)]) (parse-date d "d.m.y")))
+;; BUG in gregor, issues raised (#34 at https://github.com/97jaz/gregor/issues/34)
+;; > (parse-date "27.03.2020" "d.m.y")
+;; #<date 2020-01-27>
 
 ;; ---------------------------------------------------------------------------------------------------
 (define DATE0   "27.03.2014")
@@ -17,37 +28,34 @@
 (define RED     (make-object color% "red"))
 (define WHITE   (make-object color% "white"))
 
-(define *flight      (list-ref CHOICES 0)) ;; one of the CHOICES
+(define *kind-flight (list-ref CHOICES 0)) ;; one of the CHOICES
 (define *start-date  (to-date DATE0))      ;; date
 (define *return-date (to-date DATE0))      ;; date 
 
 (define (enable-book (start-date *start-date) (return-date *return-date))
   (send book enable #f)
   (when (and start-date (date<=? (today) start-date)
-             (or (string=? ONE *flight)
+             (or (string=? ONE *kind-flight)
                  (and return-date (date<=? start-date return-date))))
     (send book enable #t)))
 
 (define (enable-return-book . self+evt)
-  (set! *flight (list-ref CHOICES (if (empty? self+evt) 0 (send  (first self+evt) get-selection))))
-  (send return-d enable (string=? RETURN *flight))
+  (set! *kind-flight (list-ref CHOICES (if (null? self+evt) 0 (send (first self+evt) get-selection))))
+  (send return-d enable (string=? RETURN *kind-flight))
   (enable-book))
 
-(define-syntax-rule (field *date e)
-  (let ((field-cb (lambda (self evt)
-                    (define date (to-date (send self get-value)))
-                    (cond
-                      [date (set! *date date)
-                            (send self set-field-background WHITE)
-                            (enable-book)]
-                      [else (send self set-field-background RED)
-                            (enable-book #f #f)]))))
-    (new text-field% [parent frame][label ""][init-value DATE0][enabled e] [callback field-cb])))
+(define (field date-setter! enabled)
+  (define (field-cb self evt)
+    (define date (to-date (send self get-value)))
+    (cond
+      [date (send self set-field-background WHITE) (date-setter! date) (enable-book)]
+      [else (send self set-field-background RED)   (enable-book #f #f)]))
+  (new text-field% [parent frame][label ""][init-value DATE0][enabled enabled] [callback field-cb]))
 
 (define frame    (new frame% [label "flight booker"]))
 (define choice   (new choice% [label ""][parent frame][choices CHOICES][callback enable-return-book]))
-(define start-d  (field *start-date #t))
-(define return-d (field *return-date #f))
+(define start-d  (field (λ (nu) (set! *start-date nu))  #t))
+(define return-d (field (λ (nu) (set! *return-date nu)) #f))
 (define book     (new button% [label "Book"][parent frame][callback (compose displayln list)]))
 
 (enable-return-book)
