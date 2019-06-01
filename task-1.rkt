24612963258a33b8d1a606aa7925058222c275cc
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-01 18:45:23 -0400

    reduce macros to min, use h-o functions instead; make executable

--- task-1.rkt
+++ task-1.rkt
@@ -1,17 +1,18 @@
+#! /usr/bin/env gracket
 #lang racket/gui
 
 ;; a mouse-click counter
 
-(define counter-state 0)
+(define *counter 0)
 
 (define (count! . x)
-  (if (empty? x) (set! counter-state 0) (set! counter-state (+ counter-state 1)))
-  (send view set-value (~a counter-state)))
+  (set! *counter (if (empty? x)  0 (+ *counter 1)))
+  (send view set-value (~a *counter)))
 
-(define frame  (new frame% [label "Counter"]))
-(define pane   (new horizontal-pane% [parent frame]))
-(define view   (new text-field% [parent pane] [label ""] [init-value "0"][enabled #f][min-width 40]))
-(define button (new button% [parent pane] [label "Count"] [callback count!]))
+(define frame (new frame% [label "Counter"]))
+(define pane  (new horizontal-pane% [parent frame]))
+(define view  (new text-field% [parent pane][label ""][init-value "0"][enabled #f][min-width 100]))
+(define _but  (new button% [parent pane] [label "Count"] [callback count!]))
 
 (count!)
 (send frame show #t)
--- task-2.rkt
+++ task-2.rkt
@@ -1,3 +1,4 @@
+#! /usr/bin/env gracket
 #lang racket/gui
 
 ;; a bi-dorectional temperature converter (Fahrenheit vs Celsius]
@@ -5,24 +6,23 @@
 (define *C 0)
 (define *F 0)
 
-(define-syntax-rule
-  (define-callback (name field *from convert *to))
-  (define (name . x)
-    (define field:num (if (empty? x) 0 (string->number (send field get-value))))
-    (when field:num 
-      (set!-values (*from *to) (values field:num (convert field:num)))
-      (send C-field set-value (~a *C))
-      (send F-field set-value (~a *F)))))
+(define-syntax-rule (flow *from --> *to) (λ (x) (set!-values (*from *to) (values x (--> x)))))
 
-(define (field lbl cb)
-  (new text-field% [parent pane] [label lbl] [init-value ""] [callback cb]))
+(define ((callback setter) . self+evt)
+  (define field:num (if (empty? self+evt) 0 (string->number (send (first self+evt) get-value))))
+  (when field:num
+    (setter field:num)
+    (send C-field set-value (~a *C))
+    (send F-field set-value (~a *F))))
 
-(define-callback (celsius->fahrenheit C-field    *C (λ (c) (+ (* c 9/5) 32)) *F))
-(define-callback (fahrenheit->celsius F-field *F (λ (f) (* (- f 32) 5/9)) *C))
+(define (field lbl cb) (new text-field% [parent pane] [label lbl] [init-value ""] [callback cb]))
+
+(define celsius->fahrenheit (callback (flow *C (λ (c) (+ (* c 9/5) 32)) *F)))
+(define fahrenheit->celsius (callback (flow *F (λ (f) (* (- f 32) 5/9)) *C)))
 
 (define frame   (new frame% [label "temperature converter"]))
 (define pane    (new horizontal-pane% [parent frame]))
-(define C-field (field "celsius:" celsius->fahrenheit))
+(define C-field (field "celsius:"       celsius->fahrenheit))
 (define F-field (field " = fahrenheit:" fahrenheit->celsius))
 
 (celsius->fahrenheit)
