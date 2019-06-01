ccf99d2de10da831d12f28910ea3a85f75188ba5
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-01 11:48:46 -0400

    fix layout

--- task-1.rkt
+++ task-1.rkt
@@ -6,11 +6,12 @@
 
 (define (count! . x)
   (if (empty? x) (set! counter-state 0) (set! counter-state (+ counter-state 1)))
-  (send view set-label (~a counter-state)))
+  (send view set-value (~a counter-state)))
 
 (define frame  (new frame% [label "Counter"]))
-(define view   (new button% [parent frame] [label ""] [callback void]))
-(define button (new button% [parent frame] [label "Count"] [callback count!]))
+(define pane   (new horizontal-pane% [parent frame]))
+(define view   (new text-field% [parent pane] [label ""] [init-value "0"][enabled #f][min-width 40]))
+(define button (new button% [parent pane] [label "Count"] [callback count!]))
 
 (count!)
 (send frame show #t)
