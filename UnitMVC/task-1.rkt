c6312be6392f3f1c73fbfd01902bfc7d0d0eb16f
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-03 12:14:41 -0400

    ordering of unit linking

--- UnitMVC/task-1.rkt
+++ UnitMVC/task-1.rkt
@@ -17,7 +17,7 @@
     (import run^)
     (export count^)
     [define *counter 0]
-    (define (count! )
+    (define (count! . _)
       (set! *counter (+ *counter 1))
       (on-change-to-model *counter))))
   
@@ -28,11 +28,12 @@
   (unit
     (import count^)
     (export run^)
+    (init-depend count^)
 
     (define f (new frame% [label "Counter"]))
     (define p  (new horizontal-pane% [parent f]))
     (define v  (new text-field% [parent p][label ""][init-value (R 0)][enabled #f][min-width 99]))
-    (define _  (new button% [parent p] [label "Count"] [callback (λ _ (count!))]))
+    (define _  (new button% [parent p] [label "Count"] [callback count!]))
 
     (define (on-change-to-model c) (send v set-value (R c)))
     (define (run) (send f show #t))))
@@ -45,8 +46,8 @@
     (import)
     (export view)
     (link
-     (((view  : run^))  (view@ rendering) count)
-     (((count : count^)) count@ view))))
+     (((count : count^)) count@ view)
+     (((view  : run^))  (view@ rendering) count))))
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; let's run them 
@@ -59,3 +60,6 @@
   (define (nat->sticks n) (make-string n #\|))
   (define-values/invoke-unit (run@ nat->sticks) (import) (export run^))
   (run))
+
+(run-a)
+(run-sticks)
