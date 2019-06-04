7c4b69179ecc1c67cc1bee0aa982cc88c0316cfd
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-04 18:24:34 -0400

    fix bug in red-lighting illegal inputs

--- Macros/task-2.rkt
+++ Macros/task-2.rkt
@@ -5,23 +5,23 @@
 
 (require 7GUI/Macros/7guis)
 
-(define-syntax-rule (callback setter)
-  (λ (field _evt)
-    (define field:num (string->number (send field get-value)))
-    (if field:num (setter field:num) (send field set-field-background (make-object color% "red")))))
+(define ((callback flow*) field _evt)
+  (define field:num (string->number (send field get-value)))
+  (send field set-field-background (make-object color% "white"))
+  (if field:num (flow* field:num) (send field set-field-background (make-object color% "red"))))
 
 (define-syntax-rule (flow *from --> *to)
   (λ (x) (unless (= *from x) (set!-values (*from *to) (values x (--> x))))))
 
-(define ((propagate to-field) x)
-  (send to-field set-field-background (make-object color% "white"))
-  (send to-field set-value (~r x #:precision 4)))
+(define ((propagate-to field) x)
+  (send field set-field-background (make-object color% "white"))
+  (send field set-value (~r x #:precision 4)))
 
-(define celsius->fahrenheit (callback (flow *C (λ (c) (+  (* c 9/5) 32)) *F)))
-(define fahrenheit->celsius (callback (flow *F (λ (f) (* (- f 32) 5/9))  *C)))
+(define celsius->fahrenheit (callback (flow *C (λ (c) (+ (* c 9/5) 32)) *F)))
+(define fahrenheit->celsius (callback (flow *F (λ (f) (* (- f 32) 5/9)) *C)))
 
 (define temp-field% (class text-field% (super-new [min-width 200])))
 
-(gui "Temperature Converter" {(*C 0 (propagate C-field)) (*F 32 (propagate F-field))}
+(gui "Temperature Converter" {(*C 0 (propagate-to C-field)) (*F 32 (propagate-to F-field))}
      ((#:id F-field temp-field% [init-value "32"][label "fahrenheit:"][callback fahrenheit->celsius])
       (#:id C-field temp-field% [init-value "0"][label "celsius:"][callback celsius->fahrenheit])))
