6789e141498fcd3349feb179730ae71d8dd9beaa
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-05 10:55:51 -0400

    document, syntax-class checking, allow vertical guis, and simplify

--- Macros/7guis.rkt
+++ Macros/7guis.rkt
@@ -2,7 +2,20 @@
 
 (provide
  ;; SYNTAX
- #; (gui Title (state:id state:expr propagate:expr) gui-spec-elements ...)
+ #; (define-gui name:id Title:expr (state:id state:expr propagate:expr) gui-spec)
+ ;; -- defines (define state state0) ...
+ ;; -- re-defines set! for state ... so that any changes to state ... invoke propagate ...
+ ;; -- defines name to be a frame-based GUI according to the gui-spec
+ ;; 
+ #; {gui-spec    == (gui-element ...)
+     gui-element == (#:id x:id g:expr [l:id l0:expr] ...)
+                 || (g:expr [l:id l0:expr] ...) }
+ ;; a gui-element creates a widget using (new g [l l0] ...);
+ ;; if an element comes with a #:id x, it is given the name x with the same scope as name
+ define-gui
+
+ #; (gui:id Title:expr (state:id state:expr propagate:expr) gui-spec)
+ ;; like define-gui, but immediately shows the constructed frame 
  gui)
 
 ;; ---------------------------------------------------------------------------------------------------
@@ -10,17 +23,44 @@
 (require (for-syntax racket/syntax))
 
 ;; ---------------------------------------------------------------------------------------------------
+(begin-for-syntax
+  
+  (define-syntax-class field+value-expr
+    #:description "name and value binding"
+    (pattern (x:id e:expr)))
+
+  (define-syntax-class gui-element
+    #:description "gui element specification"
+    (pattern (#:id x:id widget:expr fv:field+value-expr ...))
+    (pattern (widget:expr fv:field+value-expr ...)))
+
+  (define-syntax-class gui-spec
+    #:description "gui specification"
+    (pattern (ge:gui-element ...))))
+
 (define-syntax (gui stx)
-  (syntax-parse stx 
-    [(_ Title {(state:id state0:expr f:expr) ...} visuals)
-     #:with names (retrieve-ids #'visuals)
+  (syntax-parse stx
+    [(_ Title:expr {(state:id state0:expr f:expr) ...} visuals ...)
+     #'(begin (define-gui frame Title {(state state0 f) ...} visuals ...) (send frame show #t))]))
+
+(define-syntax (define-gui stx)
+  (syntax-parse stx
+    [(_ frame-name:id Title:expr {(state:id state0:expr f:expr) ...} visuals:gui-element ...)
      #:with (state-field ...) (generate-temporaries #'(state ...))
      #'(begin
          (define-values (state-field ...) (values state0 ...))
          (define-getter/setter (state state-field f) ...)
-         (define frame (new frame% [label Title] [width 200] [height 77]))
-         (horizontal-visuals names frame visuals)
-         (send frame show #t))]))
+         (define frame-name (new frame% [label Title] [width 200] [height 77]))
+         (define pane (new vertical-pane% [parent frame-name]))
+         (setup-visuals pane (visuals))
+         ...)]
+    [(_ frame-name:id Title:expr {(state:id state0:expr f:expr) ...} visuals:gui-spec)
+     #:with (state-field ...) (generate-temporaries #'(state ...))
+     #'(begin
+         (define-values (state-field ...) (values state0 ...))
+         (define-getter/setter (state state-field f) ...)
+         (define frame-name (new frame% [label Title] [width 200] [height 77]))
+         (setup-visuals frame-name visuals))]))
 
 (define-syntax (define-getter/setter stx)
   (syntax-parse stx 
@@ -41,21 +81,17 @@
           [(#:id name . stuff) (cons #'name (loop (cdr stx)))]
           [_ (loop (cdr stx))]))))
 
-(define-syntax (horizontal-visuals stx)
+(define-syntax (setup-visuals stx)
   (syntax-parse stx 
-    [(_ (name ...) frame (gui-specs ...))
-     #:do ((define horizontal #'(new horizontal-pane% [parent frame])))
+    [(_ container (gui-specs ...))
+     #:with (name ...) (retrieve-ids #'(gui-specs ...))
+     #:do ((define horizontal #'(new  horizontal-pane% [parent container])))
      #:with pane #'pane
      #:with optionally-named-gui-elements (make-gui-elements #'pane #'(gui-specs ...))
      #`(define-values (name ...)
          (let ([pane #,horizontal])
            (let* optionally-named-gui-elements
              (values name ...))))]))
-
-(begin-for-syntax
-  (define-syntax-class field+value-expr
-    #:description "name and value binding"
-    (pattern (x:id e:expr))))
 
 (define-for-syntax (make-gui-elements parent gui-specs)
   (let loop ((gui-specs (syntax->list gui-specs)))
