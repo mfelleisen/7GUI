87e98d7cab8e78483a0edaa63e8799d37feb0c7a
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-05 10:56:09 -0400

    propagate properly

--- Macros/task-3.rkt
+++ Macros/task-3.rkt
@@ -4,11 +4,6 @@
 ;; a flight booker that allows a choice between one-way and return bookings
 ;; and, depending on the choice, a start date or a start date and an end date. 
 
-
-;; TODO:
-;; -- how to propagate changes to *start-date and *return-date
-;; -- how to get vertical panes 
-
 ;; ---------------------------------------------------------------------------------------------------
 (require gregor)
 (require 7GUI/Macros/7guis)
@@ -24,35 +19,29 @@
 (define RED     (make-object color% "red"))
 (define WHITE   (make-object color% "white"))
 
-(define (enable-book (start-date *start-date) (return-date *return-date))
+(define (enable-book (start-date *start:date))
   (send book enable #f)
   (when (and start-date (date<=? (today) start-date)
              (or (and (string=? ONE *kind-flight))
-                 (and return-date (date<=? start-date return-date))))
+                 (and *return:date (date<=? start-date *return:date))))
     (send book enable #t)))
-
-(define (enable-return-book . self+evt)
-  (set! *kind-flight (list-ref CHOICES (send (first self+evt) get-selection))))
-
-(define ((field-cb date-setter! enabled) self _evt)
-  (define date (to-date (send self get-value)))
-  (cond
-    [date (send self set-field-background WHITE) (date-setter! date) (enable-book)]
-    [else (send self set-field-background RED)   (enable-book #f #f)]))
 
 (define date-field%
   (class text-field%
     (init setter! enabled)
-    (super-new [label ""][init-value DATE0][enabled enabled] [callback (field-cb setter! enabled)])))
+    (define (field-cb self _evt)
+      (define date (to-date (send self get-value)))
+      (cond
+        [date (send self set-field-background WHITE) (setter! date)]
+        [else (send self set-field-background RED)   (enable-book #f)]))
+    (super-new [label ""][init-value DATE0][enabled enabled] [callback field-cb])))
 
 (gui "Flight Booker"
-     {(*kind-flight (list-ref CHOICES 0)
-                    (λ (*kind-flight)
-                      (send return-d enable (string=? RETURN *kind-flight))
-                      (enable-book))) ;; one of the CHOICES
-      (*start-date  (to-date DATE0)  values)     ;; date
-      (*return-date (to-date DATE0)  values)}    ;; date
-     ((choice% [label ""][choices CHOICES][callback enable-return-book])
-      (date-field% (setter! (λ (nu) (set! *start-date nu))) (enabled #t))
-      (#:id return-d date-field% (setter! (λ (nu) (set! *return-date nu))) (enabled #f))
-      (#:id book button% [label "Book"][callback (λ _ (displayln "confirmed"))])))
+     {(*kind-flight ONE (λ (kf) (send return-date enable (string=? RETURN kf)) (enable-book)))
+      (*start:date  (to-date DATE0)  (λ _ (enable-book)))
+      (*return:date (to-date DATE0)  (λ _ (enable-book)))}  
+     (choice% [label ""][choices CHOICES]
+              [callback (λ (it _) (set! *kind-flight (list-ref CHOICES (send it get-selection))))])
+     (#:id start-date  date-field% (setter! (λ (nu) (set! *start:date nu)))  (enabled #t))
+     (#:id return-date date-field% (setter! (λ (nu) (set! *return:date nu))) (enabled #f))
+     (#:id book button% [label "Book"][enabled #f][callback (λ _ (displayln "confirmed"))]))
