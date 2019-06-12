8861b5da23659f93cc0e51f2c0c7f1541580af0a
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-12 17:49:13 -0400

    read decimals properly, use exact? and rational?

--- Typed/from-string.rkt
+++ Typed/from-string.rkt
@@ -2,9 +2,13 @@
 
 (provide string->er)
 
+;; Racket should have an `exact-rational?` predicate so that it matches Typed Racket's Exact-Rational.
+
 (: string->er (String -> (U Exact-Rational False)))
 (define (string->er s)
-  (define r (string->number s))
-  ;; what is the predicate for Exact-Rational ???
-  (and r (if (real? r) (cast r Exact-Rational) #f)))
+  (define r (parameterize ([read-decimal-as-inexact #f]) (string->number s)))
+  (and r (rational? r) (exact? r) (ann r Exact-Rational)))
+
+(~r (cast (string->er "1.34") Exact-Rational) #:precision 4)
+(string->er "abc")
 
