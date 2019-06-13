545dc85aef914346a83e7b92463430bfb26314d5
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-13 10:00:30 -0400

    dependencies

--- info.rkt
+++ info.rkt
@@ -3,7 +3,15 @@
 (define collection "7GUI")
 
 (define deps
-  '("gregor"
+  '(core
+    "base"
+    "racket/gui"
+    "gui-lib"
+    "at-exp-lib"
+    "htdp-lib"
+    "typed-racket-lib"
+    "typed-racket-more"
+    "gregor"
     ))
 
 (define pkg-desc "Sources for 7GUI")
