cfdaf66f68f8b9369b15469098e73b2a6a07febe
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-18 14:30:19 -0400

    comments to explain rationale

--- Typed/double-click-canvas.rkt
+++ Typed/double-click-canvas.rkt
@@ -5,17 +5,21 @@
 (require 7GUI/Typed/sub-canvas)
 
 (define-type-canvas Canvas-Double-Click%
+  ;; missing method types from supported Canvas% Class type
   (vert-margin (->* () (Integer) Void))
   (horiz-margin (->* () (Integer) Void))
   (get-scaled-client-size (-> (Values Integer Integer)))
   (get-gl-client-size (-> (Values Integer Integer)))
 
+  ;; make new methods public: 
   (on-click (-> Natural Natural Void))
   (on-double-click (-> Natural Natural Void))
 
-  [augment (on-event  (-> (Instance Mouse-Event%) Void))]
-
+  ;; allow augmentation 
   [augment (on-click (-> Natural Natural Void))]
-  [augment (on-double-click (-> Natural Natural Void))])
+  [augment (on-double-click (-> Natural Natural Void))]
+
+  ;; also allow augmentation for on-event, which is already public 
+  [augment (on-event  (-> (Instance Mouse-Event%) Void))])
 
 (require/typed 7GUI/canvas-double-click [canvas-double-click% Canvas-Double-Click%])
