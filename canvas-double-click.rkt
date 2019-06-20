c760c86c6e4cba191014aea3e905bf83650b9a59
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-20 12:51:47 -0400

    merge x and y into evt, add call on evt

--- canvas-double-click.rkt
+++ canvas-double-click.rkt
@@ -24,24 +24,25 @@
       (inner (void) on-double-click x y)
       (on-paint))
     
-    (define-state *possible-double-click? #f
+    (define-state *single-click? #f
       (λ (pdc) (if pdc (send timer start DOUBLE-CLICK-INTERVAL) (send timer stop))))
     
-    (define *x 0)
-    (define *y 0)
+    (define *evt 0)
+    (define (call f) (f (send *evt get-x) (send *evt get-y)))
+
+    (λ x (on-click . x))
 
     (define (timer-cb)
-      (when *possible-double-click? (on-click *x *y))
-      (set! *possible-double-click? #f))
+      (when *single-click? (call (λ x (on-click . x))))
+      (set! *single-click? #f))
     (define timer (new timer% [notify-callback timer-cb]))
 
     (define/overment (on-event evt)
       (cond
         [(eq? (send evt get-event-type) 'left-down)
-         (set!-values (*x *y) (values (send evt get-x) (send evt get-y)))
-         (cond
-           [(not *possible-double-click?) (set! *possible-double-click? #t)]
-           [else (set! *possible-double-click? #f) (on-double-click *x *y)])]
+         (set! *evt evt)
+         (set! *single-click? (not *single-click?))
+         (unless *single-click? (call (λ x (on-double-click . x))))]
         [else (inner (void) on-event evt)]))
     
     (super-new)))
