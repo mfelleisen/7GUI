89c326e3f13d8ccf9aa3070b236393feca315654
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-20 11:42:48 -0400

    use define-state for double-click

--- canvas-double-click.rkt
+++ canvas-double-click.rkt
@@ -7,6 +7,8 @@
  
  canvas-double-click%)
 
+(require 7GUI/Macros/7state) ;; it improves readability 
+
 (define DOUBLE-CLICK-INTERVAL (send (new keymap%) get-double-click-interval))
 
 (define canvas-double-click%
@@ -21,32 +23,25 @@
     (define/pubment (on-double-click x y)
       (inner (void) on-double-click x y)
       (on-paint))
-
-    ;; catching 
-    (define *possible-double-click? #f) ;; when this is #f, the timer is stopped 
+    
+    (define-state *possible-double-click? #f
+      (λ (pdc) (if pdc (send timer start DOUBLE-CLICK-INTERVAL) (send timer stop))))
+    
     (define *x 0)
     (define *y 0)
 
     (define (timer-cb)
       (when *possible-double-click? (on-click *x *y))
-      (set! *possible-double-click? #f)
-      (send timer stop))
+      (set! *possible-double-click? #f))
     (define timer (new timer% [notify-callback timer-cb]))
 
     (define/overment (on-event evt)
       (cond
         [(eq? (send evt get-event-type) 'left-down)
-         (set! *x (send evt get-x))
-         (set! *y (send evt get-y))
+         (set!-values (*x *y) (values (send evt get-x) (send evt get-y)))
          (cond
-           [(not *possible-double-click?)
-            (set! *possible-double-click? #t)
-            (send timer start DOUBLE-CLICK-INTERVAL)]
-           [else
-            (set! *possible-double-click? #f)
-            (send timer stop)
-            (on-double-click *x *y)])]
+           [(not *possible-double-click?) (set! *possible-double-click? #t)]
+           [else (set! *possible-double-click? #f) (on-double-click *x *y)])]
         [else (inner (void) on-event evt)]))
     
     (super-new)))
-
