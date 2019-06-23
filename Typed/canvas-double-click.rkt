21e9641ea72bcbb1a131340fdcddb2a67808d8d2
Matthias Felleisen <matthias@ccs.neu.edu>
2019-06-23 19:36:00 -0400

    Racket-y

--- Typed/canvas-double-click.rkt
+++ Typed/canvas-double-click.rkt
@@ -6,20 +6,22 @@
 
 (define-type-canvas Canvas-Double-Click%
   ;; missing method types from supported Canvas% Class type
-  (vert-margin (->* () (Integer) Void))
-  (horiz-margin (->* () (Integer) Void))
+  (vert-margin            (->* () (Integer) Void))
+  (horiz-margin           (->* () (Integer) Void))
   (get-scaled-client-size (-> (Values Integer Integer)))
-  (get-gl-client-size (-> (Values Integer Integer)))
+  (get-gl-client-size     (-> (Values Integer Integer)))
 
   ;; make new methods public: 
-  (on-click (-> Natural Natural Void))
-  (on-double-click (-> Natural Natural Void))
+  (on-click        Click-Callback)
+  (on-double-click Click-Callback)
 
   ;; allow augmentation 
-  [augment (on-click (-> Natural Natural Void))]
-  [augment (on-double-click (-> Natural Natural Void))]
+  [augment (on-click        Click-Callback)]
+  [augment (on-double-click Click-Callback)]
 
   ;; also allow augmentation for on-event, which is already public 
   [augment (on-event  (-> (Instance Mouse-Event%) Void))])
+
+(define-type Click-Callback (-> Natural Natural Void))
 
 (require/typed 7GUI/canvas-double-click [canvas-double-click% Canvas-Double-Click%])
